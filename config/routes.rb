68cab2b4fc05116b9312ddb46aed8966a821b914
jamesyu <zetaentranceserious@gmail.com>
2015-11-22 17:21:38 +0800

    Implement user register by TDD proccess

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get '/home', to: 'businesses#index'
+  get '/register', to: 'users#new'
+  resources :users, only: [:create]
   resources :categories, only: [:show]
 end
