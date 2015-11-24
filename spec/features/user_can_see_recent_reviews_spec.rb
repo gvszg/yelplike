992791ee157f2376abee5ea24218c6748f2de4d5
jamesyu <zetaentranceserious@gmail.com>
2015-11-24 15:17:03 +0800

    User can see another user's profile page feature spec

--- spec/features/user_can_see_recent_reviews_spec.rb
+++ spec/features/user_can_see_recent_reviews_spec.rb
@@ -1,20 +1,32 @@
 require 'spec_helper'
 
 feature "User can see a list of recent reviews" do
-  scenario "user sees all reviews" do
-    cafe = Fabricate(:category, name: 'cafe')
-    joe = Fabricate(:user)
-    kohimomento = Fabricate(:business, name: 'Kohimomento', creator: joe, category: cafe)
-    mojo = Fabricate(:business, name: 'Mojocoffee', creator: joe, category: cafe)
-    
+  given(:cafe) { Fabricate(:category, name: 'cafe') }
+  given(:joe) { Fabricate(:user) }
+  given(:alice) { Fabricate(:user) }
+  given(:kohimomento) { Fabricate(:business, name: 'Kohimomento', creator: joe, category: cafe) }
+  given(:mojo) { Fabricate(:business, name: 'Mojocoffee', creator: joe, category: cafe) }
+
+  background do
     review1 = Fabricate(:review, creator: joe, business: kohimomento)
-    review2 = Fabricate(:review, creator: joe, business: mojo)
-
+    review2 = Fabricate(:review, creator: alice, business: mojo)
+    review3 = Fabricate(:review, creator: joe, business: mojo)
     sign_in
-
     visit reviews_path
-    click_link "Reviews"
+  end
 
+  scenario "user sees all reviews" do    
+    click_link "Reviews"
+    expect(page).to have_content(joe.full_name)
     expect(page).to have_content(kohimomento.name)
+    expect(page).to have_content(alice.full_name)
+    expect(page).to have_content(mojo.name)
+  end
+
+  scenario "user can see another user's profile page" do
+    first("a[href='/users/#{joe.id}']").click
+    expect(page).to have_content(joe.full_name)
+    expect(page).to have_content(kohimomento.name)
+    expect(page).to have_content(mojo.name)
   end
 end
