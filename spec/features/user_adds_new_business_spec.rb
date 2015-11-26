44a6e4fe999367a6e9db8c247ef7e065764ce85c
jamesyu <zetaentranceserious@gmail.com>
2015-11-26 22:25:19 +0800

    Fix code by Tomas' comments

--- spec/features/user_adds_new_business_spec.rb
+++ spec/features/user_adds_new_business_spec.rb
@@ -15,7 +15,7 @@ feature "User can add a business" do
     fill_in 'Address', with: '1F., No.3, Aly. 20, Ln. 33, Sec. 1, Zhongshan N. Rd., Zhongshan Dist.'
     fill_in 'Region', with: 'Taipei City'
     fill_in 'Country', with: 'Taiwan'
-    select('Coffee & Tea', :from => 'Category')
+    select 'Coffee & Tea', from: 'Category'
     click_button 'Create Business'
 
     expect(page).to have_content('Coffee Sweet')
